fae1aaa355b1d8f6a725ae5b16fc078690d6156c
Adam Martinez <adam.paul.martinez@gmail.com>
2023-06-04 12:19:54 -0400

    Reorganize and cleanup

--- setup.sh
+++ setup.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
 # basic config
-cat << EOF >> /etc/profile
-
-neofetch
-EOF
+if [[ `tail /etc/profile` != *neofetch* ]]; then
+	echo -e "\n\nneofetch\n" >> /etc/profile
+fi
 cat /etc/motd >> /etc/motd.old
 rm /etc/motd
 touch /etc/motd
@@ -15,23 +14,22 @@ apt -y upgrade
 apt -y install git vim neofetch ca-certificates curl gnupg
 
 # install docker
-sudo install -m 0755 -d /etc/apt/keyrings
+install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg
 
 echo \
   "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 apt update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+apt -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
+# add pi user to docker group
 usermod -aG docker pi
 
-sudo systemctl enable docker.service
-sudo systemctl enable containerd.service
-
+# configure basic log rotation
 cat << EOF > /etc/docker/daemon.json
 {
   "log-driver": "json-file",
@@ -41,3 +39,7 @@ cat << EOF > /etc/docker/daemon.json
   }
 }
 EOF
+
+# start the service
+systemctl enable docker.service
+systemctl enable containerd.service
